923fd69b63dbd54325b177235296e30f5103a23a
Daniella Pick <danielapick@gmail.com>
2017-11-24 16:36:27 +0200

    Implement AStar and run astarVerify. I'm not sure if the results are good, still need to make our own test to verify it works correctly.

--- astar.py
+++ astar.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 
+
 class AStar:
     cost = None
     heuristic = None
@@ -42,29 +43,79 @@ class AStar:
                 return res
 
         # Initializes the required sets
-        closed_set = set()  # The set of nodes already evaluated.
+        closed_set = []  # The list of nodes already evaluated.
         parents = {}  # The map of navigated nodes.
 
         # Save the g_score and f_score for the open nodes
         g_score = {source: 0}
         open_set = {source: self.heuristic.estimate(problem, problem.initialState)}
 
-        developed = 0
+        developed = 0  # Number of times we called succ
 
-        # TODO : Implement astar.
         # Tips:
         # - To get the successor states of a state with their costs, use: problem.expandWithCosts(state, self.cost)
         # - You should break your code into methods (two such stubs are written below)
         # - Don't forget to cache your result between returning it - TODO
+        while open_set:
+            next_state = self._get_open_state_with_lowest_f_score(open_set)
+            del open_set[next_state]
+            closed_set.append(next_state)
+            if problem.isGoal(next_state):
+                # TODO : VERY IMPORTANT: must return a tuple of (path, g_score(goal), h(I), developed)
+                return (self._reconstruct_path(parents, next_state),
+                        self._calculate_path_cost(parents, g_score, next_state),
+                        self.heuristic.estimate(problem, problem.initialState),
+                        developed)
+            succ_list = problem.expandWithCosts(next_state, self.cost)
+            developed += 1
+            for succ_state, succ_state_cost in succ_list:
+                new_g = g_score[next_state] + succ_state_cost
+                if open_set.__contains__(succ_state):  # Checks if the son node was already in OPEN
+                    if new_g < g_score[succ_state]:  # Checks if found a better path for the node
+                        g_score[succ_state] = new_g
+                        parents[succ_state] = next_state
+                        open_set[succ_state] = g_score[succ_state] + self.heuristic.estimate(problem, succ_state)
+                else:
+                    if closed_set.__contains__(succ_state):  # Checks if the son node was already in CLOSED
+                        if new_g < g_score[succ_state]:  # Checks if found a better path for the node
+                            g_score[succ_state] = new_g
+                            parents[succ_state] = next_state
+                            closed_set.remove(succ_state)
+                            open_set[succ_state] = g_score[succ_state] + self.heuristic.estimate(problem, succ_state)
+                    else:  # Found a new node
+                        g_score[succ_state] = new_g
+                        parents[succ_state] = next_state
+                        open_set[succ_state] = g_score[succ_state] + \
+                                               self.heuristic.estimate(problem, succ_state)
 
-        # TODO : VERY IMPORTANT: must return a tuple of (path, g_score(goal), h(I), developed)
-        return ([], -1, -1, developed)
-
-    def _getOpenStateWithLowest_f_score(self, open_set):
-        # TODO : Implement
-        raise NotImplementedError
+    @staticmethod
+    def _get_open_state_with_lowest_f_score(open_set: dict):
+        min_f = float("inf")
+        min_state_f = 0
+        for state, state_f_value in open_set.items():
+            if state_f_value < min_f:
+                min_f = state_f_value
+                min_state_f = state
+        return min_state_f
 
     # Reconstruct the path from a given goal by its parent and so on
-    def _reconstructPath(self, parents:list, goal):
-        # TODO : Implement
-        raise NotImplementedError
+    @staticmethod
+    def _reconstruct_path(parents: dict, goal) -> list:
+        path_list = []
+        while goal:
+            path_list.insert(0, goal)
+            goal = parents.get(goal)
+        return path_list
+
+    @staticmethod
+    def _calculate_path_cost(parents: dict, g_score: dict, goal) -> int:
+        path_cost = 0
+        while goal:
+            path_cost += g_score[goal]
+            goal = parents.get(goal)
+        return path_cost
+
+
+
+
+
