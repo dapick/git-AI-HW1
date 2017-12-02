48e912936d63a28cb0a2db740d0d4e0a547acc58
Daniella Pick <danielapick@gmail.com>
2017-12-02 18:30:43 +0200

    Implemented g heuristic - question 25

--- heuristics/TSPCustomHeuristic.py
+++ heuristics/TSPCustomHeuristic.py
@@ -1,14 +1,31 @@
 from heuristics import Heuristic
 
-# TODO : Implement as explained in the instructions
-class TSPCustomHeuristic(Heuristic):
-    _distMat = None
-    _junctionToMatIdx = None
+from problems import MapProblem
+from astar import AStar
+from heuristics import L2DistanceHeuristic
 
-    # TODO : You can add parameters if you need them
-    def __init__(self, roads, initialState):
+
+# TODO : Done
+class TSPCustomHeuristic(Heuristic):
+    roads = None
+    map_astar = None
+
+    # TODO : Done
+    def __init__(self, roads):
         super().__init__()
+        self.roads = roads
+        self.map_astar = AStar(L2DistanceHeuristic(), shouldCache=True)
 
     # Estimate heuristically the minimal cost from the given state to the problem's goal
     def estimate(self, problem, state):
-        raise NotImplementedError
+        max_distance = 0
+        for order_i in state.waitingOrders:
+            source = order_i[0]
+            for order_j in state.waitingOrders:
+                target = order_j[1]
+                orders_problem = MapProblem(self.roads, source, target)
+                _, orders_distance, _, _ = self.map_astar.run(orders_problem)
+
+                if orders_distance > max_distance:
+                    max_distance = orders_distance
+        return max_distance
